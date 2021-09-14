f9b3fe97072f99c3444d8077eb5d3f8de1003e44
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-14 06:45:51 -0400

    Added:FibonaicSeries

---
+++ src/com/bridgelabs/logicalProblems.java
@@ -0,0 +1,21 @@
+package com.bridgelabs;
+
+public class logicalProblems {
+
+    // method for compute the fibonace series
+    public static void fibonaceSeries(){
+        int a=0,b=1,c,num=10;
+        System.out.println(a+" "+b);
+        for(int i=2;i<num;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+    public static void main(String args[]){
+        //calling the method in class
+        fibonaceSeries();
+    }
+}
+
