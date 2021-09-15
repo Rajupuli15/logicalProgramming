72f2b23afe8c5668f21d31fde5133eeb0a7bc3db
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-15 03:56:39 -0400

    Added:Time

--- src/com/bridgelabs/logicalProblems.java
+++ src/com/bridgelabs/logicalProblems.java
@@ -3,7 +3,6 @@ package com.bridgelabs;
 import java.util.Scanner;
 
 public class logicalProblems {
-
     // method for compute the fibonace series
     public static void fibonaceSeries() {
         int a = 0, b = 1, c, num = 10;
@@ -30,15 +29,17 @@ public class logicalProblems {
     //method to check the prime number compute
     static void primeNumber(int num1) {
         int x = num1 / 2;
+        boolean flag = false;
         for (int i = 1; i < x; i++) {
             if (num1 % i == 0) {
                 System.out.println(num1 + "number is not prime number");
-
+                flag = true;
             } else {
-
+                flag = false;
             }
-
+            if (flag == true) {
                 System.out.println(num1 + "number is prime number");
+            }
         }
     }
     static void reverseDigit ( int num1){
@@ -48,17 +49,19 @@ public class logicalProblems {
             reverse = reverse * 10 + rem;
             num1 = num1 / 10;
         }
-        System.out.println("reverse: " + reverse);
+        System.out.println("reverse" + reverse);
+    }
+    public static void watchTime(){
+        long start = System.nanoTime();
+        long stop = System.nanoTime();
+        System.out.println(start-stop+"elapse watch time");
     }
 
     public static void main (String args[]){
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the number");
-        int num1 = input.nextInt();
-
-        reverseDigit(num1);
+        watchTime();
 
     }
 
