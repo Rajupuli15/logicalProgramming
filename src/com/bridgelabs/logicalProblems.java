b144f0dd2db2dc11a56215e3928db1c483555566
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-15 03:15:45 -0400

    Added:PrimenumberAddedToGetOutput

--- src/com/bridgelabs/logicalProblems.java
+++ src/com/bridgelabs/logicalProblems.java
@@ -3,8 +3,8 @@ package com.bridgelabs;
 import java.util.Scanner;
 
 public class logicalProblems {
-
     // method for compute the fibonace series
+
     public static void fibonaceSeries(){
         int a=0,b=1,c,num=10;
         System.out.println(a+" "+b);
@@ -25,21 +25,35 @@ public class logicalProblems {
         }
         return sum;
     }
+    //method to check the prime number compute
+    static void primeNumber(int num1) {
+        int x = num1 / 2;
+        boolean flag = false;
+        for (int i = 1; i < x; i++) {
+            if (num1 % i == 0) {
+                System.out.println(num1 + "number is not prime number");
+                flag = true;
+            }
+            else {
+                flag = false;
+            }
+            if(flag == false){
+                System.out.println(num1+"number is prime number");
+            }
+        }
+    }
 
     public static void main(String args[]){
-        //calling the method in class
-        long number1;
+        //call the method in class
+        //  long number1;
         Scanner input = new Scanner(System.in);
+
+
         System.out.println("Enter the number");
-        number1 = input.nextLong();
-        //calling the perfect method and result will be in the result.
-        long result = perfect(number1);
-        //fibonaceSeries();
-        //checking the number is equal to the quotient sum
-        if(number1 == result)
-            System.out.println(number1+"number perfect number");
-        else
-            System.out.println(number1+"number not the perfect number");
+        int num1 = input.nextInt();
+        primeNumber(num1);
+
     }
+
 }
 
